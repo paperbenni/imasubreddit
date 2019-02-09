410cbd9305f5d43f44ae4bb82320edf4f61f6106
paperbenni <paperbenni@gmail.com>
2019-02-09 22:45:55 +0100

    adds cuda 9 installer

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
