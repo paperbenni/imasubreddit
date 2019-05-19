7a2496fe4e09944bdd27185fbf44ca9d3bb89a26
paperbenni <paperbenni@gmail.com>
2019-05-19 13:39:38 +0200

    unite finish

--- utilities/unite.sh
+++ utilities/unite.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-for folder in ./*; do
-    if ! ls "$folder"/; then
-        continue
-    fi
-    pushd folder
-    cat *.txt >> ../train.txt
-    popd
-done
-cat train.txt | grep -v 'https' | egrep -v '\[deleted\]'
+source <(curl -s https://raw.githubusercontent.com/paperbenni/bash/master/import.sh)
+pb grep
+
+mkdir temp
+mv * temp/
+cd temp
+cat * >> ../train.txt
+cd ..
+
+regexfilter train.txt 'https' '^.{4,200}$'
+
+cat train.txt | egrep -o ':;:.*' > train.to
+cat train.txt | egrep -o '.*:;:' > train.from
+
+sed -i -e 's/:;://g' train.to
+sed -i -e 's/:;://g' train.from
